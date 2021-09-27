6686ee4616deea1d2d10652308ff0a4f1e94ebd7
Ron Vergis <ron.vergis@gmail.com>
2021-09-26 21:10:48 -0500

    Update DebugService.js

--- javascript/examples/grapheditor/www/js/DebugService.js
+++ javascript/examples/grapheditor/www/js/DebugService.js
@@ -1,10 +1,29 @@
 function DebugService(editorUI)
 {
     this.editorUI = editorUI;
-    setTimeout(connectionBackgroundProcess, 1000, this.editorUI, 1);
+    setTimeout(sendSourceCodeToUnityEditor, 1000, this.editorUI, 1);
+    //setTimeout(pingUnityEditor, 1000, this.editorUI, 1);
 }
 
-function connectionBackgroundProcess(editorUI, counter)
+function pingUnityEditor(editorUI, counter)
+{
+    const xmlHTTP = new XMLHttpRequest();
+    xmlHTTP.open('POST', 'http://127.0.0.1:10002/ping', true);
+    xmlHTTP.onreadystatechange = function() { // Call a function when the state changes.
+        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+            // Request finished. Do processing here.
+            // show the unity toolbar
+        }
+        else
+        {
+            // hide the unity toolbar
+        }
+        setTimeout(pingUnityEditor, 3000, this.editorUI, ++counter);
+    }
+    xmlHTTP.send();
+}
+
+function sendSourceCodeToUnityEditor(editorUI, counter)
 {
     // attempt a connection to the Unity HTTP Server
     // if this succeeds, add the Unity refresh button to toolbar
@@ -24,7 +43,7 @@ function connectionBackgroundProcess(editorUI, counter)
     }
     else
     {
-        setTimeout(connectionBackgroundProcess, 1000, editorUI, ++counter);
+        setTimeout(sendSourceCodeToUnityEditor, 1000, editorUI, ++counter);
     }
 }
 
